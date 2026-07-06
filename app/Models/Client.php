47bc72817542c92b07e520f6e5ef74ffdd69f633
Cristian31306 <est_ca_duran@fesc.edu.co>
2026-07-06 16:37:16 -0500

    mayusculas en formularios

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Str;
 
 #[Fillable(['name', 'phone', 'document_id'])]
 class Client extends Model
 {
+    /**
+     * Mutador para guardar el nombre siempre en mayúscula.
+     */
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            set: fn (?string $value) => $value ? Str::upper($value) : $value,
+        );
+    }
+
     public function invoices()
     {
         return $this->hasMany(Invoice::class);
